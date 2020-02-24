b016c07436b28073d548119d830bb4a8d5b57f4f
Alejandro Obando Cano <alejandro_obando@outlook.com>
2020-02-23 23:42:00 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res=0;
+            Console.WriteLine("Ingrese la operacion");
+            string input = Console.ReadLine();
+            if (input.Contains('+'))
+            {
+                var digitos = input.Split('+');
+                res = int.Parse(digitos[0]) + int.Parse(digitos[1]);
+            }
+            else if (input.Contains('-'))
+            {
+                var digitos = input.Split('-');
+                res = int.Parse(digitos[0]) - int.Parse(digitos[1]);
+            }
+            else if (input.Contains('*'))
+            {
+                var digitos = input.Split('*');
+                res = int.Parse(digitos[0]) * int.Parse(digitos[1]);
+            }
+            else if (input.Contains('/'))
+            {
+                var digitos = input.Split('/');
+                res = int.Parse(digitos[0]) / int.Parse(digitos[1]);
+            }
+            else Console.WriteLine("Operacion invalida");
+
+            Console.WriteLine("El resultado es: " + res);
+            Console.ReadKey();
+        }
+    }
+}
